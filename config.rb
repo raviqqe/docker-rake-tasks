7ed6f4a9abb2233074ab36bc9e58722e4a0070ec
Yota Toyama <raviqqe@gmail.com>
2016-10-23 06:07:26 +0900

    Add configuration file

---
+++ config.rb
@@ -0,0 +1,10 @@
+DOCKER_USER = 'raviqqe'
+
+WEB_PORT = 8000
+GIT_PORT = 8001
+
+DOMAIN_NAME = 'raviqqe.com'
+
+# cert_dir = File.join '/usr/local/etc/letsencrypt/live', DOMAIN_NAME
+# CERT_PATH = File.join cert_dir, 'fullchain.pem'
+# PRIV_KEY_PATH = File.join cert_dir, 'privkey.pem'
